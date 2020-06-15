46c53fb94d1e489aabaada20dd1b7188b23c5506
unknown <panchi.psp@gmail.com>
2020-06-15 16:02:28 +0900

    まとめて import utaupy as up することにした。

--- oto2lab.py
+++ oto2lab.py
@@ -16,7 +16,8 @@ from glob import glob
 from pprint import pprint
 from shutil import copy2, move
 
-from utaupy import convert, label, otoini, table, ust
+# from utaupy import convert, label as _label, otoini as _otoini, table, ust as _ust
+import utaupy as up
 
 # from pathlib import Path
 # from pprint import pprint
@@ -76,12 +77,12 @@ def ustfile_to_inifile_solo(path_ustfile, outdir, path_tablefile, mode='romaji_c
     # 'outdir/<name>.ini'
     print('converting UST to INI :', path_ustfile)
     # UST を読み取り
-    ustobj = ust.load(path_ustfile)
-    # ustobj.replace_lyrics('息', 'br')
-    # ustobj.replace_lyrics('R', 'pau')
+    ust = up.ust.load(path_ustfile)
+    # ust.replace_lyrics('息', 'br')
+    # ust.replace_lyrics('R', 'pau')
     # 変換
-    otoiniobj = convert.ust2otoini(ustobj, name_wav, path_tablefile)
-    otoiniobj.write(path_inifile)
+    otoini = up.convert.ust2otoini(ust, name_wav, path_tablefile)
+    otoini.write(path_inifile)
     print('converted  UST to INI :', path_inifile)
     return path_inifile
 
@@ -121,9 +122,9 @@ def inifile_to_labfile_solo(path_inifile, outdir, mode='auto'):
     path_labfile = '{}/{}'.format(outdir, basename.replace('.ini', '.lab'))
     print('converting INI to LAB :', path_inifile)
     # INI を読み取り
-    o = otoini.load(path_inifile)
+    o = up.otoini.load(path_inifile)
     # 変換
-    lab = convert.otoini2label(o, mode=mode, debug=DEBUG_MODE)
+    lab = up.convert.otoini2label(o, mode=mode, debug=DEBUG_MODE)
     # LAB を書き出し
     lab.write(path_labfile)
     print('converted  INI to LAB :', path_labfile)
@@ -166,9 +167,9 @@ def labfile_to_inifile_solo(path_labfile, outdir):
     name_wav = basename.replace('.lab', '.wav')
     # 変換開始
     print('converting LAB to INI :', path_labfile)
-    lab = label.load(path_labfile)
-    o = convert.label2otoini(lab, name_wav)
-    o.write(path_inifile)
+    label = up.label.load(path_labfile)
+    otoini = up.convert.label2otoini(label, name_wav)
+    otoini.write(path_inifile)
     print('converted  LAB to INI :', path_inifile)
 
 
@@ -215,18 +216,18 @@ def inifile_kana2romaji(path, path_tablefile):
         print('\n出力ファイル上書き回避のため、既存INIファイルを移動します。')
         path_backup = backup_files(outdir, 'ini')
         print('移動先:', path_backup)
-    d = table.load(path_tablefile)
+    d = up.table.load(path_tablefile)
     # ファイル変換処理
     for p in l:
-        otoiniobj = otoini.load(p)
-        for oto in otoiniobj.values:
+        otoini = up.otoini.load(p)
+        for oto in otoini.values:
             try:
                 oto.alias = ' '.join(d[oto.alias])
             except KeyError as e:
                 print('[WARNING] KeyError in oto2lab.inifile_kana2ramaji')
                 print('  詳細:', e)
         print(p)
-        otoiniobj.write(p)
+        otoini.write(p)
 
 
 # def run_ExecuteUstToOto(path_xlsm):
