0ed6530c3e94db0f27a7b7e13364bdd8ff9e7abe
unknown <panchi.psp@gmail.com>
2020-06-20 02:57:23 +0900

    Create generate_label_from_xml.py
    muxicxml からモノラベルとフルラベルを生成するスクリプトを追加

---
+++ tool/generate_label_from_xml.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""
+about module
+"""
+
+from glob import glob
+
+import pysinsy
+
+
+def main():
+    p = input('musicxmlがあるフォルダのPATHを入力してね : ')
+    xmlfiles = glob(f'{p}/**/*.musicxml')
+    xmlfiles += glob(f'{p}/**/*.xml')
+    for v in xmlfiles:
+        print(v)
+    input('waiting')
+    for xml in xmlfiles:
+        generate_label(xml)
+
+
+def generate_label(xmlpath):
+    print(xmlpath)
+
+    sinsy = pysinsy.sinsy.Sinsy()
+    # Set language to Japanese
+    assert sinsy.setLanguages("j", "/usr/local/lib/sinsy/dic")
+    assert sinsy.loadScoreFromMusicXML(xmlpath)
+
+    is_mono = True
+    labels = sinsy.createLabelData(is_mono, 1, 1).getData()
+    with open(xmlpath + '.lab', 'w') as f:
+        f.writelines(labels)
+
+    is_mono = False
+    labels = sinsy.createLabelData(is_mono, 1, 1).getData()
+    with open(xmlpath + '.full', 'w') as f:
+        f.writelines(labels)
+
+    sinsy.clearScore()
+
+
+if __name__ == '__main__':
+    main()
