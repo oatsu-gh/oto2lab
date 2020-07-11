78783f1086f263aaa4ebd03d27b57ff552cfbfcb
unknown <panchi.psp@gmail.com>
2020-07-11 15:05:20 +0900

    Create audacitylabel2lab.py
    Audacityのラベル（txt, 時間単位s, タブ区切り）をモノフォンラベル（lab, 時間単位100ns, 空白区切り）にするツールを作った。

---
+++ tool/audacitylabel2lab.py
@@ -0,0 +1,48 @@
+#!python3
+# coding: utf-8
+# Copyright (c) 2020 oatsu
+"""
+txt (Audacityのラベル) -> lab (歌唱DBモノフォンラベル) の変換
+読み取って、時間単位と区切り文字変えて保存するだけ。
+"""
+import os
+from glob import glob
+from pprint import pprint
+from sys import exit
+
+import utaupy as up
+
+
+def audacitylabelfile2labfile(inpath, outpath):
+    """
+    txt (Audacityのラベル) -> lab (歌唱DBモノフォンラベル)
+    単一ファイルを変換
+    """
+    label = up.label.load(inpath, kiritan=True)
+    lines = label.write(outpath, delimiter='\t')
+
+
+def main():
+    p = input('Input Audacity label txt path (file or dir)\n>>> ')
+    if os.path.isdir(p):
+        txtfiles = glob('{}/*.{}'.format(p, 'txt'))
+    else:
+        if not p.endswith('.txt'):
+            print('[ERROR] txt ではないファイルが入力されました。終了します。')
+            input('Press Enter to exit')
+            exit()
+        txtfiles = [p]
+    print('\n処理対象ファイル')
+    pprint(txtfiles)
+    print('\n始めます')
+    for path_txt in txtfiles:
+        print('  processing:', path_txt)
+        path_lab = path_txt.replace('.txt', '.lab')
+        audacitylabelfile2labfile(path_txt, path_lab)
+    print('終わりました')
+
+
+if __name__ == '__main__':
+    print('---Audacity用ラベルを歌唱DB用ラベルにするツール(20200711)---')
+    main()
+    print('Press Enter to exit')
