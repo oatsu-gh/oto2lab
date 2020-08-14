a137a9b45e538cdd0ffa2b34c5d51a1bb9bab04c
oatsu <panchi.psp@gmail.com>
2020-08-15 01:49:50 +0900

    Update generate_label_from_xml.py
    入力パスの変換を修正。ファイル名を変更。

--- tool/generate_label_from_xml.py
+++ tool/generate_label_from_xml.py
@@ -20,12 +20,12 @@ def generate_label(xmlpath):
 
     is_mono = True
     labels = sinsy.createLabelData(is_mono, 1, 1).getData()
-    with open(xmlpath + '.lab', 'w') as f:
+    with open(xmlpath.replace('.musicxml', '.lab').replace('.xml', '.lab'), 'w') as f:
         f.writelines(labels)
 
     is_mono = False
     labels = sinsy.createLabelData(is_mono, 1, 1).getData()
-    with open(xmlpath + '.full', 'w') as f:
+    with open(xmlpath.replace('.musicxml', '.lab').replace('.xml', '.lab'), 'w') as f:
         f.writelines(labels)
 
     sinsy.clearScore()
@@ -33,7 +33,7 @@ def generate_label(xmlpath):
 
 def main():
     p = input('musicxmlがあるフォルダのPATHを入力してね : ').strip('"')
-    p = p.replace('C:\\', 'mnt/c/').replace('D:\\', 'mnt/d/').replace('D:\\', 'mnt/e/')
+    p = p.replace('C:\\', '/mnt/c/').replace('D:\\', 'mnt/d/').replace('D:\\', 'mnt/e/')
     p = p.replace('\\', '/')
 
     xmlfiles = glob(f'{p}/**/*.musicxml', recursive=True)
