092455334afa19936808c38429dfa522263dad63
oatsu <panchi.psp@gmail.com>
2020-08-17 13:02:30 +0900

    Update 配布用zipをつくるやつ.bat

--- 配布用zipをつくるやつ.bat
+++ 配布用zipをつくるやつ.bat
@@ -1,12 +1,16 @@
-rmdir /s /q exe\oto2lab-exe
+rem oto2labをexeにして配布用フォルダを作るバッチ
 
-mkdir exe\oto2lab-exe\
-copy /Y oto2lab.py exe\oto2lab-exe\oto2lab.py
-copy /Y README.md exe\oto2lab-exe\readme.md
-xcopy /Y /q table exe\oto2lab-exe\table\
+rmdir /s /q exe\oto2lab--exe
 
-cd exe\oto2lab-exe
-pyinstaller --onefile oto2lab.py --exclude-module read
+mkdir exe\oto2lab--exe\
+copy /Y oto2lab.py exe\oto2lab--exe\oto2lab.py
+copy /Y README.md exe\oto2lab--exe\readme.txt
+xcopy /Y /q table exe\oto2lab--exe\table\
+ren exe\oto2lab--exe\table\README.md readme.txt
+copy /Y exe\oto2labGUI.exe exe\oto2lab--exe\oto2labGUI.exe
+
+cd exe\oto2lab--exe
+pyinstaller --onefile oto2lab.py
 
 move /Y dist\oto2lab.exe oto2lab.exe
 rmdir /s /q dist, build, __pycache__
