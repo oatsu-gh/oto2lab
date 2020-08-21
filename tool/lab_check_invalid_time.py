eaa2174dee163e05e099eda51f7d753bc56f76f7
oatsu <panchi.psp@gmail.com>
2020-08-21 21:39:03 +0900

    Create lab_check_invalid_time.py
    ラベルの前後関係をチェックするやつ

---
+++ tool/lab_check_invalid_time.py
@@ -0,0 +1,23 @@
+#!python
+# coding: utf-8
+# Copyright (c) oatsu
+"""
+発声区間がマイナス値なラベルがないか検査
+"""
+from glob import glob
+import utaupy as up
+
+def main():
+    path_lab_dir = input('path_lab_dir: ')
+    l_path_lab = glob(f'{path_lab_dir}/**/*.lab', recursive=True)
+    for path_lab in l_path_lab:
+        print(path_lab)
+        label = up.label.load(path_lab)
+        for phoneme in label.values:
+            duration = phoneme.end - phoneme.start
+            if duration <= 0:
+                print(f'  [ERROR] 発声時間がゼロ以下です : {phoneme.start} {phoneme.end} {phoneme.symbol}')
+
+if __name__ == '__main__':
+    main()
+    input('Press Enter to exit.')
