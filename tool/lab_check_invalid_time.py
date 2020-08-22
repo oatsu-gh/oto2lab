433d1027deedd5e142e16ab849079babe9830441
oatsu <panchi.psp@gmail.com>
2020-08-23 02:25:02 +0900

    Update lab_check_invalid_time.py
    エラー前後の音素がわかるようにした

--- tool/lab_check_invalid_time.py
+++ tool/lab_check_invalid_time.py
@@ -5,7 +5,9 @@
 発声区間がマイナス値なラベルがないか検査
 """
 from glob import glob
+
 import utaupy as up
+
 
 def main():
     path_lab_dir = input('path_lab_dir: ')
@@ -13,10 +15,11 @@ def main():
     for path_lab in l_path_lab:
         print(path_lab)
         label = up.label.load(path_lab)
-        for phoneme in label.values:
+        for i, phoneme in enumerate(label.values):
             duration = phoneme.end - phoneme.start
             if duration <= 20000:
-                print(f'  [ERROR] 発声時間が2ms未満です : {phoneme.start} {phoneme.end} {phoneme.symbol}')
+                print(f'  [ERROR] 発声時間が2ms未満です : {phoneme.start} {phoneme.end} ({label.values[i-1].symbol}) {phoneme.symbol} ({label.values[i+1].symbol})')
+
 
 if __name__ == '__main__':
     main()
