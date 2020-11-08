e2eac0b6aa336a9e816348061e7c1fa23a383623
oatsu <panchi.psp@gmail.com>
2020-11-09 03:22:34 +0900

    Update lab_insert_sil.py
    音素整合性チェックに機能を追加し、エラー内容がわかりやすくした

--- tool/lab_insert_sil/lab_insert_sil.py
+++ tool/lab_insert_sil/lab_insert_sil.py
@@ -28,18 +28,24 @@ def check_label_diff_for_debug(labobj_oto2lab, labobj_sinsy):
     """
     oto2lab_sil = [phoneme for phoneme in labobj_oto2lab if phoneme.symbol == 'sil']
     sinsy_sil = [phoneme for phoneme in labobj_sinsy if phoneme.symbol == 'sil']
+    oto2lab_pau = [phoneme for phoneme in labobj_oto2lab if phoneme.symbol == 'pau']
+    sinsy_pau = [phoneme for phoneme in labobj_sinsy if phoneme.symbol == 'pau']
     oto2lab_br = [phoneme for phoneme in labobj_oto2lab if phoneme.symbol == 'br']
     sinsy_br = [phoneme for phoneme in labobj_sinsy if phoneme.symbol == 'br']
     oto2lab_cl = [phoneme for phoneme in labobj_oto2lab if phoneme.symbol == 'cl']
     sinsy_cl = [phoneme for phoneme in labobj_sinsy if phoneme.symbol == 'cl']
     print('  oto2labのラベルの音素数   :', len(labobj_oto2lab))
     print('  Sinsy  のラベルの音素数   :', len(labobj_sinsy))
+    print('  oto2labのラベル中のpauの数:', len(oto2lab_pau))
+    print('  Sinsy  のラベル中のpauの数:', len(sinsy_pau))
     print('  oto2labのラベル中のsilの数:', len(oto2lab_sil))
     print('  Sinsy  のラベル中のsilの数:', len(sinsy_sil))
     print('  oto2labのラベル中のbrの数 :', len(oto2lab_br))
     print('  Sinsy  のラベル中のbrの数 :', len(sinsy_br))
     print('  oto2labのラベル中のclの数 :', len(oto2lab_cl))
     print('  Sinsy  のラベル中のclの数 :', len(sinsy_cl))
+    assert len(oto2lab_br) == len(sinsy_br), 'br の個数が一致しません。'
+    assert len(labobj_oto2lab) - len(oto2lab_pau) - len(oto2lab_sil) == len(labobj_sinsy) - len(sinsy_pau) - len(sinsy_sil), 'pau, sil 以外の音素で登録ミスが存在するようです。'
 
 
 def delete_pau_and_sil(labobj):
@@ -115,7 +121,7 @@ def main_wrap(path_lab_oto2lab, path_lab_sinsy, path_lab_out):
     # 前処理
     labobj_oto2lab = utaupy.label.load(path_lab_oto2lab)
     labobj_sinsy = utaupy.label.load(path_lab_sinsy)
-    print('  処理前------------------------')
+    print('  処理前-------------------------')
     check_label_diff_for_debug(labobj_oto2lab, labobj_sinsy)
     # oto2labのラベルのsilを全部消す
     delete_pau_and_sil(labobj_oto2lab)
@@ -123,7 +129,7 @@ def main_wrap(path_lab_oto2lab, path_lab_sinsy, path_lab_out):
     insert_pau_and_sil(labobj_oto2lab, labobj_sinsy)
     # pauが失った時間情報を復元する
     restore_pau_time(labobj_oto2lab)
-    print('  処理後------------------------')
+    print('  処理後-------------------------')
     check_label_diff_for_debug(labobj_oto2lab, labobj_sinsy)
     labobj_oto2lab.check_invalid_time()
     # ファイル出力
@@ -142,11 +148,11 @@ def main():
     # LABファイルを取得
     sinsy_labels = glob(f'{path_sinsy_label_dir}/*.lab')
     oto2lab_labels = glob(f'{path_sinsy_label_dir}/*.lab')
-    len_sinsny_labels = len(sinsy_labels)
+    len_sinsy_labels = len(sinsy_labels)
     len_oto2lab_labels = len(oto2lab_labels)
-    print('len_sinsny_labels :', len_sinsny_labels)
-    print('len_oto2lab_labels:', len_sinsny_labels)
-    assert len_sinsny_labels == len_oto2lab_labels, 'sinsyのラベルファイル数とoto2labのラベルファイル数が一致しません。'
+    print('len_sinsy_labels  :', len_sinsy_labels)
+    print('len_oto2lab_labels:', len_sinsy_labels)
+    assert len_sinsy_labels == len_oto2lab_labels, 'sinsyのラベルファイル数とoto2labのラベルファイル数が一致しません。'
     # 書き換えを始める。
     for path_lab_sinsy in sinsy_labels:
         print('--------------------------------------------------')
