fc6694bf12e445f71bf2b1b1eca5a86ffa8a6330
oatsu-gh <panchi.psp@gmail.com>
2021-02-06 17:54:34 +0900

    書類まわりを修正

--- 配布用zipをつくるやつ.bat
+++ 配布用zipをつくるやつ.bat
@@ -5,9 +5,9 @@ rmdir /s /q exe\oto2lab--exe
 mkdir exe\oto2lab--exe\
 copy /Y oto2lab.py exe\oto2lab--exe\oto2lab.py
 copy /Y README.md exe\oto2lab--exe\readme.txt
-copy /Y README-English.md exe\oto2lab--exe\readme.txt
-xcopy /Y /q table exe\oto2lab--exe\table\
-ren exe\oto2lab--exe\table\README.md readme.txt
+copy /Y README_English.md exe\oto2lab--exe\readme_english.txt
+xcopy /Y /q dic exe\oto2lab--exe\dic\
+ren exe\oto2lab--exe\dic\README.md readme.txt
 copy /Y exe\oto2labGUI.exe exe\oto2lab--exe\oto2labGUI.exe
 
 cd exe\oto2lab--exe
